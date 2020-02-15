9015267d6a965bb064f88d9dc8c4966d41ef4d21
Jiang Yuxin <jyx11011@gmail.com>
2020-02-15 17:24:04 +0800

    bfs

--- CS3243_P1_31_1.py
+++ CS3243_P1_31_1.py
@@ -1,19 +1,136 @@
 import os
 import sys
+import copy
 
+from collections import deque
 
 class Puzzle(object):
+
     def __init__(self, init_state, goal_state):
         # you may add more attributes if you think is useful
         self.init_state = init_state
         self.goal_state = goal_state
-        self.actions = list()
+        self.actions = [(1,0),(-1,0),(0,-1),(0,1)]
+        self.actionName = ["UP", "DOWN", "RIGHT", "LEFT"]
+        self.initStateString = list()
+
+        self.n = len(init_state)
+        self.size = n*n
+        self.visited = dict()
+
+    def getBlank(self, state):
+        for i in range(self.n):
+            for j in range(self.n):
+                if state[i][j]==0:
+                    return (i,j)
+
+    def numToPair(self, num):
+        return (num/n, num%n)
+
+    def pairToNum(self, p):
+        return p[0]*n+p[1]
+
+    def isValid(self, i, j):
+        return i>=0 and i<self.n and j>=0 and j<self.n
+
+    def getActions(self):
+        state = tuple(item for row in self.goal_state for item in row)
+        (blankX, blankY) = self.getBlank(self.goal_state)
+        actionList=[]
+        while self.visited[state] != -1:
+
+            action = self.visited[state]
+            actionList.append(self.actionName[action])
+            (prevX, prevY) = (blankX - self.actions[action][0], blankY - self.actions[action][1])
+            prevState = list(state)
+            prevState[self.pairToNum((prevX, prevY))]=0
+            prevState[self.pairToNum((blankX,blankY))]=state[self.pairToNum((prevX, prevY))]
+            state = tuple(prevState)
+            blankX = prevX
+            blankY = prevY
+
+        actionList.reverse()
+        if not self.checkActions(actionList):
+          print("Wrong")
+        return actionList
+
+    def checkActions(self, actionList):
+      state = copy.deepcopy(self.init_state)
+      for action in actionList:
+        (x,y) = self.getBlank(state)
+        if (action == "UP"):
+          # move the bottom cell upwards =
+          state[x][y]= state[x+1][y]
+          state[x+1][y]=0
+        elif (action == "DOWN"):
+          # move the top cell downwards 
+          state[x][y]=state[x-1][y]
+          state[x-1][y]=0
+        elif (action == "LEFT"):
+          # move the right cell leftwards
+          state[x][y] = state[x][y+1]
+          state[x][y+1] = 0
+        elif (action == "RIGHT"):
+          #move the left cell rightwards 
+          state[x][y]=state[x][y-1]
+          state[x][y-1]=0
+      return state == self.goal_state
+
+    def bfs(self):
+        queue = deque()
+        goal_tuple = tuple(item for row in self.goal_state for item in row)
+        init_tuple = tuple(item for row in self.init_state for item in row)
+        self.visited[init_tuple]=-1
+        queue.append((init_tuple, 0, self.getBlank(init_state)))
+        while queue:
+            head = queue.popleft()
+            state= head[0]
+            (x, y) = head[2]
+            step = head[1]
+            for i in range(4):
+                (nextX, nextY) = (x + self.actions[i][0], y + self.actions[i][1])
+                if not self.isValid(nextX, nextY):
+                    continue
+                nextState = list(state)
+                nextState[self.pairToNum((nextX, nextY))]=0
+                nextState[self.pairToNum((x,y))]=state[self.pairToNum((nextX, nextY))]
+                nextState = tuple(nextState)
+                if not self.visited.get(nextState) is None:
+                    continue
+                self.visited[nextState] = i
+                if nextState == goal_tuple:
+                    queue.clear()
+                    return self.getActions()
+                queue.append((nextState, step+1, (nextX, nextY)))
+        
+        return ['UNSOLVABLE']
+        
+
+    def isSolvable(self):
+        numInvert = self.getNumOfInversions();
+        return (numInvert % 2 == 0)
+    
+    def getNumOfInversions(self):
+        # create the string of interest for comparison 
+        for row in range(len(init_state)):
+            for col in range(len(init_state)):
+                self.initStateString.append(self.init_state[row][col]);
+        # start counting the inversions. 
+        invCount = 0;
+        for i in range(len(self.initStateString)):
+            for j in range(i+1, len(self.initStateString)):
+                if (self.initStateString[j] > 0 and self.initStateString[i] > self.initStateString[j]): 
+                    invCount += 1;
+        return invCount;
 
     def solve(self):
         #TODO
         # implement your search algorithm here
-        
-        return ["LEFT", "RIGHT"] # sample output 
+        if (self.isSolvable()):
+          #then call BFS 
+          return self.bfs();
+        else:
+          return ["UNSOLVABLE"]
 
     # you may add more functions if you think is useful
 
@@ -66,10 +183,3 @@ if __name__ == "__main__":
     with open(sys.argv[2], 'a') as f:
         for answer in ans:
             f.write(answer+'\n')
-
-
-
-
-
-
-
